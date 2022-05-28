a6652031434eefed6edbf1acbed832afb66b407d
GrayFlash <gk74533@gmail.com>
2022-05-28 08:53:39 +0530

    chore(node-todo-app): cleanup unneccessary files

--- node-app/gray-todo-react/src/App.js
+++ node-app/gray-todo-react/src/App.js
@@ -1,16 +1,8 @@
-import logo from './logo.svg';
-
-function App(props) {
-  const greet = "hey folks!!"
+function App() {
   return (
     <div className="App">
       <header className="App-Header">
-        {/* <img src={logo} className="App-logo" alt="logo"/> */}
         <h1>TODO APP INIT</h1>
-        <br/>
-        <p>
-          {greet}, and {props.subject} wanna start the meeting??
-        </p>
       </header>
     </div>
   );
